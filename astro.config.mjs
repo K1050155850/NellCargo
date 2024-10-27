d4fab71b065f7fd4391fcb68d665293772c036d4
K1050155850 <lemastalin344@gmail.com>
2024-10-26 20:59:56 -0500

    Configuracion de sitio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
+    site: 'https://K1050155850.github.io',
+    base: 'NellCargo',
 });
